322eb5cbad741ff92680df2c9f424710b55f1006
brianmujjuni <brianmujjuni@engineer.com>
2025-07-28 15:17:34 +0300

    change font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ import "react-native-reanimated";
 export default function RootLayout() {
 
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
   });
 
   if (!loaded) {
