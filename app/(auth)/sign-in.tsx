75e7f1d124b92d79acfba443bee3fef77f1c2f8c
brianmujjuni <brianmujjuni@engineer.com>
2025-07-28 17:14:20 +0300

    change main screen to welcome screen

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignIn = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignIn Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
