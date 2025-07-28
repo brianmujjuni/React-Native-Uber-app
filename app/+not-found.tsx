5cf570ba88816b51d09a22ef034889f2f9f92e10
brianmujjuni <brianmujjuni@engineer.com>
2025-07-28 14:49:49 +0300

    add asets

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,19 +1,17 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+// If you have a custom ThemedText component, import it instead:
+// import ThemedText from '../components/ThemedText';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
+       <Stack.Screen options={{title: "Ops!"}}/>
+      <View style={styles.container}>
+        <Text>Ops! This screen doesn't exist.</Text>
+        <Link style={styles.link} href="/">Go to home Screen</Link>
+      </View>
     </>
   );
 }
