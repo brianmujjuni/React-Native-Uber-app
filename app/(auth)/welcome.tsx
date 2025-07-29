11bb1282a129ceb3b9ff0c21dfeb7b7cf093360b
brianmujjuni <brianmujjuni@engineer.com>
2025-07-30 02:00:42 +0300

    create onboarding screens

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,3 +1,4 @@
+import CustomButon from "@/components/customButton";
 import { onboarding } from "@/constants";
 import { router } from "expo-router";
 import { useRef, useState } from "react";
@@ -8,6 +9,7 @@ import "../../global.css";
 const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
   const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
 
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
@@ -41,14 +43,22 @@ const Onboarding = () => {
               <Text className="text-black text-3xl font-bold mx-10">
                 {item.title}
               </Text>
-              
             </View>
             <Text className="text-lg font-JakartaBold text-center text-[#858585] mx-10 mt-3">
-                {item.description}
-              </Text>
+              {item.description}
+            </Text>
           </View>
         ))}
       </Swiper>
+      <CustomButon
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10 "
+      />
     </SafeAreaView>
   );
 };
