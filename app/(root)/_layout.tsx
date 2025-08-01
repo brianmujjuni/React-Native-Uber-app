5d43da69c1db56bdbe788d4f4dbad23cd7a7fb55
brianmujjuni <brianmujjuni@engineer.com>
2025-08-02 02:14:57 +0300

    create signin logic

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -2,5 +2,7 @@
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return <Stack >
+    <Stack.Screen name='(tabs)' options={{headerShown: false}}/>
+  </Stack>;
 }
