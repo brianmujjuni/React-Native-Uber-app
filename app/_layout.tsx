0698737a45bf9d0c16a8bcfd6881f777047c183c
brianmujjuni <brianmujjuni@engineer.com>
2025-08-01 13:41:21 +0300

    wrap rootLayout with tokenCache

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import "react-native-reanimated";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY; // Replace with your actual publishable key
+
+
+
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -27,7 +31,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ClerkProvider publishableKey={publishableKey}>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <ClerkLoaded>
         {/* <Slot /> */}
 
