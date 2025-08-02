4f6aa904f25047da6dfe4a6418f91c4d1ad946d1
brianmujjuni <brianmujjuni@engineer.com>
2025-08-02 16:10:35 +0300

    create http method to post or create users

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -1,3 +1,20 @@
-import { neon } from '@neondatabase/serverless';
+import { neon } from "@neondatabase/serverless";
 
-const sql = neon(process.env.DaTABASE_URL || '');
+export async function Post(request: Request) {
+  try {
+    const sql = neon(`${process.env.DaTABASE_URL}`);
+    const { name, email, clerkId } = await request.json();
+    if (!name || !email || !clerkId) {
+      return Response.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+    const response =
+      await sql`insert into users (name, email, clerk_id) values (${name}, ${email}, ${clerkId}) returning *`;
+    return new Response(JSON.stringify({ data: response }));
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: error }, { status: 500 });
+  }
+}
