1dd088c3091934aae62593ba081be3084ad2076b
brianmujjuni <brianmujjuni@engineer.com>
2025-08-04 13:18:03 +0300

    destracure ride data

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -2,10 +2,19 @@ import { Ride } from "@/types/type";
 import React from "react";
 import { Text, View } from "react-native";
 
-function RideCard({ ride }: { ride: Ride }) {
+function RideCard({ ride:{
+    destination_longitude,
+    destination_latitude,
+    origin_address,
+    destination_address,
+    created_at,
+    ride_time,
+    driver,
+    payment_status
+} }: { ride: Ride }) {
   return (
     <View>
-      <Text className="text-3xl">{ride.driver.first_name}</Text>
+      <Text className="text-3xl">{driver.first_name}</Text>
     </View>
   );
 }
